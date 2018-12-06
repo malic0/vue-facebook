8ada6638ed12e0b50ec8efc7764455c3da253774
Malico <malico.yong@gmail.com>
2018-12-06 02:18:50 +0100

    use User's URL

--- src/components/_fb-comment.vue
+++ src/components/_fb-comment.vue
@@ -1,31 +1,32 @@
 <template>
-    <div class="fb-comments" :data-colorscheme="colorsheme" :data-mobile="mobile" :data-href="'https://developers.facebook.com/docs/plugins/comments#configurator'" :data-numposts="numposts" :data-order-by="orderBy">
-    </div>
+  <div class="fb-comments" :data-colorscheme="colorsheme" :data-mobile="mobile" :data-href="url" :data-numposts="numposts" :data-order-by="orderBy">
+  </div>
 </template>
 <script>
 export default {
 
 
-    props: {
-        href: {
-            type: String
-        },
-        mobile: {
-            type: Boolean,
-            default: false
-        },
-        numposts: {
-            type: Number,
-            default: 10
-        },
-        orderBy: {
-            type: String,
-            default: "social"
-        },
-        colorsheme: {
-            type: String,
-            default: 'light'
-        }
+  props: {
+    url: {
+      type: String
+    },
+    mobile: {
+      type: Boolean,
+      default: false
+    },
+    numposts: {
+      type: Number,
+      default: 10
+    },
+    orderBy: {
+      type: String,
+      default: "social"
+    },
+    colorsheme: {
+      type: String,
+      default: 'light'
     }
+  }
 }
+
 </script>
